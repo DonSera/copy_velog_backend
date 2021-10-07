7922452bf27f909d032963f5d65f6fd318775f0d
DonSera <sera.2021@lifeoasis.com>
2021-10-07 16:04:24 +0900

    feat : 로그인 구현 - /login 일때 - 이메일 존재 먼저 확인 - 비밀번호 일치 확인 - status와 message를 담겨 보낸다.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
+import * as console from "console";
 
 const express = require('express');
 const cors = require('cors');
@@ -14,38 +15,51 @@ const corsOptions = {
     optionsSuccessStatus: 200,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
-}
+};
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 createConnection().then(async connection => {
-    let userEmail = '';
-    let userId = 0;
+    app.post('/login', async (req, res) => {
+        const userInfo = {
+            email: undefined,
+            name: undefined,
+            status: false,
+            message: ""
+        };
 
-    app.get('/button', (req, res) => {
-        userId = req.query.id;
-        User.findOne({id: userId})
-            .then(userObj => {
-                userEmail = userObj.email
-                res.json({email: userEmail, message: "Get email success"})
-            })
-            .catch(err => {
-                console.log('Can not find email by id')
-                console.log(err)
-                res.json({email: undefined, message: "Can not find email by id"})
-            });
+        const inputEmail = req.body.email;
+        const inputPassword = req.body.password;
+
+        try {
+            const userObj = await User.findOne({email: inputEmail})
+            if (userObj.password === inputPassword) {
+                // 성공 했을 때만 이메일과 이름을 넣어 보낸다.
+                userInfo.email = userObj.email
+                userInfo.name = userObj.name;
+                userInfo.status = true;
+                userInfo.message = "Login success";
+            } else {
+                userInfo.message = "password miss match";
+            }
+        } catch (err) {
+            userInfo.message = 'email miss match';
+            console.log(err);
+        }
+        console.log(userInfo.message);
+        res.json(userInfo);
     });
 
     app.get('/', async function (req, res) {
         res.send("get '/' server");
-    })
+    });
 
     app.listen(port, () => {
         console.log('####Express listening on port####', port);
     });
 
 }).catch(error => {
-    console.log(error)
+    console.log(error);
 });
