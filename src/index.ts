8289f92cc063655d43d713694854c27250f5b70e
DonSera <sera.2021@lifeoasis.com>
2021-10-07 12:15:51 +0900

    feat : /button 들어오면 작동 - id에 맞는 email 값 가져오기 - 값이 있다면 메세지와 client에 넘겨주기 - 값이 없다면 undefined와 메세지 넘겨주기

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,45 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
-import * as console from "console";
 
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-createConnection().then(async connection => {
-    connection.manager.find(User).then(user =>{
-        console.log(user);
-        }
-    )
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+}
 
-    // router.post("/", (req, res) => {
-    //     const {id} = req.body;
-    //     if (id.valid()) {
-    //         res.json({message: "OK"})
-    //     } else {
-    //         res.json({message: "Not OK ..."})
-    //     }
-    // })
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+createConnection().then(async connection => {
+    let userEmail = '';
+    let userId = 0;
+
+    app.get('/button', (req, res) => {
+        userId = req.query.id;
+        User.findOne({id: userId})
+            .then(userObj => {
+                userEmail = userObj.email
+                res.json({email: userEmail, message: "Get email success"})
+            })
+            .catch(err => {
+                console.log('Can not find email by id')
+                console.log(err)
+                res.json({email: undefined, message: "Can not find email by id"})
+            });
+    });
 
     app.get('/', async function (req, res) {
-        res.send('hello~');
+        res.send("get '/' server");
     })
 
     app.listen(port, () => {
