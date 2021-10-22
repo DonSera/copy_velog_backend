3717d410ca7fcad726554922195266b6a4519c9e
DonSera <sera.2021@lifeoasis.com>
2021-10-22 19:43:09 +0900

    style : 주석 추가

--- src/entity/PostTag.ts
+++ src/entity/PostTag.ts
@@ -3,16 +3,18 @@ import {Post} from "./Post";
 import {Tag} from "./Tag";
 
 @Entity('postTag')
-export class PostTag extends BaseEntity{
-    @PrimaryColumn({type:"int"})
-    postId : number;
+export class PostTag extends BaseEntity {
+    @PrimaryColumn({type: "int"})
+    postId: number;
 
     @PrimaryColumn({type: "varchar"})
     tagTag: string;
 
-    @OneToOne(() => Post, post => post.id)
-    post : Post
+    // 일자 추가 해야 order 가능..
 
-    @OneToOne(() =>Tag, tag =>tag.tag)
+    @OneToOne(() => Post, post => post.id)
+    post: Post
+
+    @OneToOne(() => Tag, tag => tag.tag)
     tag: Tag;
 }
